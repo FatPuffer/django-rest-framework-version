4a6659d50261ad0a6b2ab9c555dd408e81364bfa
dc@163.com <flask@163.com>
2019-04-17 17:49:59 +0800

    '创建模型类'

--- dj_restful_version/api/models.py
+++ dj_restful_version/api/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class UserGroup(models.Model):
+    title = models.CharField(max_length=32)
+
+
 class UserInfo(models.Model):
     USER_TYPE_CHOICES = (
         (1, '普通用户'),
@@ -11,9 +15,14 @@ class UserInfo(models.Model):
     username = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=64)
 
+    group = models.ForeignKey("UserGroup")
+    roles = models.ManyToManyField("Role")
+
 
 class UserToken(models.Model):
     user = models.OneToOneField(to='UserInfo')
     token = models.CharField(max_length=64)
 
 
+class Role(models.Model):
+    title = models.CharField(max_length=32)
