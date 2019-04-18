dd524ee6c0ae7f05b826dbcb1c8417d27d0bc4c7
dc@163.com <flask@163.com>
2019-04-18 17:04:16 +0800

    '反向生成url'

--- dj_restful_version/api/views.py
+++ dj_restful_version/api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse, HttpResponse
 from rest_framework .views import APIView
 from rest_framework.versioning import QueryParameterVersioning, URLPathVersioning
+from django.urls import reverse
 
 
 # class ParamVersion(object):
@@ -30,5 +31,18 @@ from rest_framework.versioning import QueryParameterVersioning, URLPathVersionin
 class UsersView(APIView):
 
     def get(self, request, *args, **kwargs):
+        # 获取版本
         print(request.version)
+
+        # 获取处理版本的对象
+        print(request.versioning_scheme)
+
+        # 反向生成 URL （基于 rest framework）
+        url = request.versioning_scheme.reverse(viewname='xxx', request=request)
+        print(url)
+
+        # 反向生成 URL （基于 Django）
+        url2 = reverse(viewname='xxx', kwargs={'version': 2})
+        print(url2)
+
         return HttpResponse('用户列表')
