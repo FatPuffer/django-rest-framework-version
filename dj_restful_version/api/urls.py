76312d0c70b50f50bcb66c1284fe5b8f058fa23f
dc@163.com <flask@163.com>
2019-04-19 14:27:23 +0800

    继承Serializer实现序列化

--- dj_restful_version/api/urls.py
+++ dj_restful_version/api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^(?P<version>[v1|v2]+)/users/$', UsersView.as_view(), name='xxx'),
     url(r'^(?P<version>[v1|v2]+)/parser/$', ParserView.as_view()),
+    url(r'^(?P<version>[v1|v2]+)/roles/$', RoleView.as_view()),
+    url(r'^(?P<version>[v1|v2]+)/userinfo/$', UserInfoView.as_view()),
 ]
--- dj_restful_version/api/views.py
+++ dj_restful_version/api/views.py
@@ -3,6 +3,8 @@ from rest_framework .views import APIView
 from rest_framework.versioning import QueryParameterVersioning, URLPathVersioning
 from django.urls import reverse
 
+from .models import Role, UserInfo
+
 
 # class ParamVersion(object):
 #
@@ -72,4 +74,51 @@ class ParserView(APIView):
         print(request.data['age'])
         return HttpResponse('用户信息')
 
+
+import json
+
+
+class RoleView(APIView):
+    """自己实现序列化JSON"""
+    def get(self, request, *args, **kwargs):
+        roles = Role.objects.all().values('id', 'title')
+        roles = list(roles)
+        # ensure_ascii=False：关闭自动编码，使其可以直接呈现中文汉字
+        ret = json.dumps(roles, ensure_ascii=False)
+
+        return HttpResponse()
+
+
+from rest_framework import serializers
+
+
+class UserInfoSerializer(serializers.Serializer):
+    """继承序列化对象实现序列化"""
+    # 获取choice选项中的编号
+    aaa = serializers.CharField(source='user_type')
+    # 获取choice选项中的值 get_字段名_display
+    bbb = serializers.CharField(source='get_user_type_display')
+    username = serializers.CharField()
+    password = serializers.CharField()
+    # 显示外键group表中的id和title字段
+    gp = serializers.CharField(source="group.title")
+    # SerializerMethodField：自定义显示，实现嵌套序列化
+    rls = serializers.SerializerMethodField()
+
+    # 自定义显示
+    def get_rls(self, row):
+        role_obj_list = row.roles.all()
+        ret = []
+        for item in role_obj_list:
+            ret.append({"id": item.id, "title": item.title})
+        return ret
+
+
+class UserInfoView(APIView):
+    def get(self, request, *args, **kwargs):
+        user = UserInfo.objects.all()
+        ser = UserInfoSerializer(instance=user, many=True)
+
+        ret = json.dumps(ser.data, ensure_ascii=False)
+        return HttpResponse(ret)
 
